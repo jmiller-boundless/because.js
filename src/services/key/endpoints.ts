847456dc829e787bda799895168541e9d19a4554
Joseph Miller <jmiller@boundlessgeo.com>
2018-01-09 15:00:08 -0500

    added key and org management endpoints and parsing

--- src/services/key/endpoints.ts
+++ src/services/key/endpoints.ts
@@ -10,5 +10,53 @@ export const endpoints = {
               "apikey": args.key.toString()
           });
       }
+  ),
+  "create_key": new Endpoint(
+    "POST", "/auth/admin/create-apikey",
+    undefined,
+    undefined,
+    (args) => {
+      return JSON.stringify({
+        "organizationId":args.organizationId,
+        "expireQuantity":args.expireQuantity,
+        "expireUnit":args.expireQuantity,
+        "roles":args.roles
+      });
+    }
+  ),
+  "update_key": new Endpoint(
+    "POST","/auth/admin/update-apikey",
+    undefined,
+    undefined,
+    (args) =>{
+      return JSON.stringify({
+        "id":args.id,
+        "expireQuantity":args.expireQuantity,
+        "roles":args.roles,
+        "expireUnit":args.expireUnit
+      });
+    }
+  ),
+  "get_organizations": new Endpoint(
+      "GET", "/auth/admin/get-organizations"
+  ),
+  "get_organization_byid": new Endpoint(
+    "GET", "/auth/admin/get-organization-by-name/{name}"
+  ),
+  "delete_organization_byid": new Endpoint(
+    "GET", "/auth/admin/delete-organization-by-id/{id}"
+  ),
+  "update_organization": new Endpoint(
+    "POST", "/auth/admin/update-organization",
+    undefined,
+    undefined,
+    (args) => {
+      return JSON.stringify({
+        "id":args.id,
+        "name":args.name
+      });
+    }
   )
+
+
 }
--- src/services/key/parse.ts
+++ src/services/key/parse.ts
@@ -10,12 +10,6 @@ import { parse_response } from "../../parse";
  * /src/main/java/com/boundlessgeo/bcs/data/BCSToken.java
  */
 export class KeyValidateData {
-    //organization: string;
-    //key: string;
-    //roles: string[];
-    //errorCode: number | undefined;
-    //errorMessage: string | undefined;
-
     constructor (
       public organization: string,
       public key: string,
@@ -25,6 +19,54 @@ export class KeyValidateData {
     ){
 
     }
+}
+export class UserRole{
+  constructor (
+    public id: number,
+    public key: string,
+    public description: string
+  ){}
+}
+export class ApiKey {
+    constructor (
+      public id: number,
+      public key: string,
+      public created: number,
+      public expires: number,
+      public authorizedRoles: UserRole[],
+      public parentOrganizationId: number,
+      public errorCode: number| undefined,
+      public errorMessage: string| undefined
+    ){
+
+    }
+}
+
+export class User{
+  constructor(
+    public id: number,
+    public email: string,
+    public roles: UserRole[],
+    public created: number,
+    public parentOrganizationId: number
+  ){
+
+  }
+}
+
+export class Organization{
+  constructor(
+    public id: number,
+    public name: string,
+    public created: number,
+    public members: User[],
+    public administrators: User[],
+    public apiKeys: ApiKey[],
+    public errorCode: number| undefined,
+    public errorMessage: string| undefined
+  ){
+
+  }
 }
 
 export function parse_key_validate(response: Response): KeyValidateData {
@@ -38,5 +80,72 @@ export function parse_key_validate(response: Response): KeyValidateData {
           data.errorCode,
           data.errorMessage
         );
+}
+
+export function parse_ApiKey(response: Response): ApiKey{
+  const data = parse_response<ApiKey>(response);
+  const roles = [];
+  for (const role_data of data.authorizedRoles) {
+      const role = new UserRole(role_data.id,role_data.key,role_data.description);
+      roles.push(role);
+  }
+  return new ApiKey(
+    data.id,
+    data.key,
+    data.created,
+    data.expires,
+    roles,
+    data.parentOrganizationId,
+    data.errorCode,
+    data.errorMessage
+  );
+
+}
+
+export function parse_Organization(response: Response): Organization{
+  const data = parse_response<Organization>(response);
+  const members = [];
+  for (const memberdata of data.members) {
+      const memberroles = [];
+      for(const memberrole of memberdata.roles){
+        const role = new UserRole(memberrole.id,memberrole.key,memberrole.description);
+        memberroles.push(role);
+      }
+      const member = new User(memberdata.id,memberdata.email,memberroles,memberdata.created,memberdata.parentOrganizationId);
+      members.push(member);
+  }
+
+  const administrators = [];
+  for (const administordata of data.administrators) {
+      const adminroles = [];
+      for(const adminrole of administordata.roles){
+        const role = new UserRole(adminrole.id,adminrole.key,adminrole.description);
+        adminroles.push(role);
+      }
+      const administrator = new User(administordata.id,administordata.email,adminroles,administordata.created,administordata.parentOrganizationId);
+      administrators.push(administrator);
+  }
+
+  const apiKeys = [];
+  for (const apikeydata of data.apiKeys){
+    const roles = [];
+    for (const role_data of apikeydata.authorizedRoles) {
+        const role = new UserRole(role_data.id,role_data.key,role_data.description);
+        roles.push(role);
+    }
+    const key = new ApiKey(
+      apikeydata.id,apikeydata.key,apikeydata.created,apikeydata.expires,roles,apikeydata.parentOrganizationId,apikeydata.errorCode,apikeydata.errorMessage);
+    apiKeys.push(key);
+  }
+  return new Organization(
+    data.id,
+    data.name,
+    data.created,
+    members,
+    administrators,
+    apiKeys,
+    data.errorCode,
+    data.errorMessage
+  );
 
 }
